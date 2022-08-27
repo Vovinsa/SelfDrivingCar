d8f4b2724e49d6d9188a11ccbdacfcec29d778b4
Vladimir <vi142696@gmail.com>
2022-08-27 15:04:35 +0300

    feat: add speed control

--- car/utils/dcmotor.py
+++ car/utils/dcmotor.py
@@ -8,23 +8,36 @@ class DCMotor:
 
     """
     def __init__(self):
+        GPIO.setmode(GPIO.TEGRA_SOC)
         self._ENA = "GPIO_PE6"
         self._IN1 = "DAP4_FS"
         self._IN2 = "SPI2_MOSI"
-        GPIO.setmode(GPIO.TEGRA_SOC)
+
         GPIO.setup(self._ENA, GPIO.OUT, initial=GPIO.LOW)
+        self.pwm = GPIO.PWM(self._ENA, 120)
+        self.pwm.start(0)
+
         GPIO.setup(self._IN1, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self._IN2, GPIO.OUT, initial=GPIO.LOW)
 
     def stop(self):
-        GPIO.output(self._ENA, GPIO.HIGH)
+        self.pwm.stop()
         GPIO.output(self._IN1, GPIO.LOW)
         GPIO.output(self._IN2, GPIO.LOW)
 
-    def backward(self):
+    def forward(self, speed):
+        self._change_duty_cycle(speed)
         GPIO.output(self._IN1, GPIO.HIGH)
         GPIO.output(self._IN2, GPIO.LOW)
 
-    def forward(self):
+    def backward(self, speed):
+        self._change_duty_cycle(speed)
         GPIO.output(self._IN1, GPIO.LOW)
         GPIO.output(self._IN2, GPIO.HIGH)
+
+    def _change_duty_cycle(self, dc):
+        if dc > 100:
+            dc = 100
+        elif dc < 60:
+            dc = 60
+        self.pwm.ChangeDutyCycle(dc)
