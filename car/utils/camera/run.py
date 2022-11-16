47401156d65697dc59df46ac4d95f87222990db7
Vladimir <vi142696@gmail.com>
2022-11-16 18:27:54 +0300

    fix: kafka port

--- car/utils/camera/run.py
+++ car/utils/camera/run.py
@@ -2,15 +2,17 @@ from kafka import KafkaProducer
 
 from camera import Camera
 
-producer = KafkaProducer(
-    bootstrap_servers=["localhost:29092"]
-)
-cam = Camera(capture_width=1280, capture_height=720,
-             display_width=224, display_height=224, framerate=30)
 
-while True:
-    frame = cam.update()
-    producer.send(
-        "camera",
-        {"frame": frame}
+if __name__ == "__main__":
+    producer = KafkaProducer(
+        bootstrap_servers=["kafka:9092"]
     )
+    cam = Camera(capture_width=1280, capture_height=720,
+                 display_width=224, display_height=224, framerate=30)
+
+    while True:
+        frame = cam.update()
+        producer.send(
+            "camera",
+            {"frame": frame}
+        )
