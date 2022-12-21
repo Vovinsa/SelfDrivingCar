970544a31648c1740b789131ab2a9cbdcc2b114e
Vladimir <vi142696@gmail.com>
2022-12-21 14:39:20 +0300

    fix: dataset returning items

--- ml/dataset.py
+++ ml/dataset.py
@@ -7,19 +7,20 @@ import os
 
 
 class CarDataset(Dataset):
-    def __init__(self, df, transform=None):
+    def __init__(self, df, path, transform=None):
         super(CarDataset, self).__init__()
         self.transform = transform
         self.df = df.copy()
+        self.path = path
 
     def __getitem__(self, item):
-        img_name, rot_angle, command = self.df.iloc[item].values()
+        img_name, rot_angle, speed = self.df.iloc[item].values
         img = cv2.imread(os.path.join(self.path, img_name))
         if self.transform:
             img = self.transform(img)
-        rot_angle = torch.IntTensor(rot_angle)
-        command = torch.IntTensor(command)
-        return img, rot_angle, command
+        measurements = torch.FloatTensor([rot_angle, speed])
+        command = 0
+        return img, measurements, command
 
     def __len__(self):
         return len(self.df)
