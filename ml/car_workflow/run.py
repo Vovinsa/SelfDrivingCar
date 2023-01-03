5ec9f883d08af1f2add07aca63006542bb4e750b
Vladimir <vi142696@gmail.com>
2023-01-03 22:05:44 +0300

    fix: work on car

--- ml/car_workflow/run.py
+++ ml/car_workflow/run.py
@@ -8,29 +8,46 @@ import pickle
 import json
 
 
+import time
+
+
 if __name__ == "__main__":
     producer = KafkaProducer(
-        boostrap_servers=["kafka:9092"],
+        bootstrap_servers=["localhost:29092"],
         value_serializer=lambda x: json.dumps(x).encode("ascii"),
         api_version=(0, 10, 2)
     )
     consumer = KafkaConsumer(
         "camera",
-        bootsrap_servers=["kafka:9092"],
-        value_deserializer=lambda x: pickle.loads(x.decode("ascii")),
+        bootstrap_servers=["localhost:29092"],
+        value_deserializer=lambda x: pickle.loads(x),
         api_version=(0, 10, 2)
     )
 
-    ort_sess = ort.InferenceSession("../models/onnx/model.onnx", providers=["CUDAExecutionProvider"])
-    ort_inp = {
-        "input.img": np.random.rand(1, 3, 224, 224).astype(np.float32),
-        "input.meas": np.array([[25, 0]], dtype=np.float32)
-    }
+    ort_sess = ort.InferenceSession("../models/onnx/model.onnx", providers=["TensorrtExecutionProvider"])
+    print("A")
+
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+
     for msg in consumer:
-        frame = msg["frame"]
+        s = time.time()
+        frame = msg.value["frame"]
+        frame = frame / 255
+        frame = np.transpose(frame, (2, 0, 1))
+        frame = (frame - mean[:, None, None]) / std[:, None, None]
+        frame = np.expand_dims(frame, 0)
+
+        ort_inp = {
+            "input.img": frame.astype(np.float32),
+        }
         ort_out = ort_sess.run(["output.angle", "output.speed"], ort_inp)
-        angle, speed = ort_out[0], ort_out[1]
+
+        angle, speed = ort_out[0].round().item(), ort_out[1].round().item()
+        print(angle, speed)
+
         producer.send(
             "motors",
             value={"rotation_angle": angle, "speed": speed}
         )
+        print(time.time() - s)
