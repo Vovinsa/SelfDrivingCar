7c720e9baa73cf4a412d28e064c1c139c57fd422
Vladimir <vi142696@gmail.com>
2023-01-17 00:48:10 +0300

    refactor: add operations

--- car/utils/camera/run.py
+++ car/utils/camera/run.py
@@ -7,11 +7,11 @@ import pickle
 
 if __name__ == "__main__":
     producer = KafkaProducer(
-        bootstrap_servers=["kafka:9092"],
+        bootstrap_servers=["localhost:29092"],
         value_serializer=lambda x: pickle.dumps(x),
         api_version=(0, 10, 2)
     )
-    cam = Camera(capture_width=1280, capture_height=720,
+    cam = Camera(capture_width=1640, capture_height=1232,
                  display_width=224, display_height=224, framerate=30)
 
     while True:
