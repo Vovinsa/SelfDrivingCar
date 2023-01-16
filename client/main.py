d81d11140a11e4ac357cecdaa39019de9eaee394
Vladimir <vi142696@gmail.com>
2023-01-17 00:47:34 +0300

    refactor: change server

--- client/main.py
+++ client/main.py
@@ -7,18 +7,6 @@ import json
 sio = socketio.Client()
 
 
-@sio.event
-def connect():
-    print("I'm connected!")
-    sio.emit("chat", {"rotate": 0, "speed": 0, "car_id": 2, "is_car": True})
-
-
-@sio.event
-def connect_error():
-    print("The connection failed!")
-    sio.connect("http://134.17.144.36:5000", wait_timeout=10)
-
-
 @sio.on("chat")
 def on_message(data):
     angle = data["rotate"]
@@ -26,19 +14,8 @@ def on_message(data):
 
     producer.send(
         "motors",
-        value={"speed": speed, "rotation_angle": angle}
+        value={"speed": int(speed), "rotation_angle": int(angle)}
     )
-
-
-@sio.event
-def disconnect():
-    print("disconnect")
-    try:
-        sio.disconnect()
-        sio.connect("http://134.17.144.36:5000", wait_timeout=10)
-    except:
-        print("zxc")
-        sio.connect("http://134.17.144.36:5000", wait_timeout=10)
 
 
 if __name__ == "__main__":
@@ -47,5 +24,5 @@ if __name__ == "__main__":
         value_serializer=lambda x: json.dumps(x).encode("ascii"),
         api_version=(0, 10, 2)
     )
-    sio.connect("http://134.17.144.36:5000", wait_timeout=10)
+    sio.connect("http://192.168.100.9:3000", wait_timeout=10)
     sio.wait()
