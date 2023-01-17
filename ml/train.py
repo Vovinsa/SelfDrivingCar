dc3ba21f2bb5684ee8108dd98567945bf23c9ab1
Vladimir <vi142696@gmail.com>
2023-01-17 12:31:31 +0300

    feat: remove normalize

--- ml/train.py
+++ ml/train.py
@@ -73,8 +73,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+        transforms.ToTensor()
     ])
 
     try:
